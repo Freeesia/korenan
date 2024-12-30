28652cf70dfe1d77a759b9412459e71ed54ad196
Freesia <freesia@studiofreesia.com>
2024-12-30 21:03:25 +0900

    いったんプロジェクト追加

--- korenan.AppHost/Program.cs
+++ korenan.AppHost/Program.cs
@@ -6,4 +6,14 @@ builder.AddProject<Projects.korenan_Web>("webfrontend")
     .WithExternalHttpEndpoints()
     .WithReference(apiService);
 
+var weatherApi = builder.AddProject<Projects.korenan_react_Server>("korenan-react-server");
+
+builder.AddNpmApp("react", "../korenan.react/korenan.react.client", "dev")
+    .WithReference(weatherApi)
+    .WaitFor(weatherApi)
+    .WithEnvironment("BROWSER", "none") // Disable opening browser on npm start
+    .WithHttpsEndpoint(env: "VITE_PORT")
+    .WithExternalHttpEndpoints()
+    .PublishAsDockerFile();
+
 builder.Build().Run();
