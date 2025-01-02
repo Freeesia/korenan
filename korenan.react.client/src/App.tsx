f2469c449b64df58836c78b1d339fe6b1d4655d5
Freesia <freesia@studiofreesia.com>
2025-01-02 18:27:51 +0900

    デバッグページ追加

--- korenan.react.client/src/App.tsx
+++ korenan.react.client/src/App.tsx
@@ -1,14 +1,31 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import Weather from "./pages/Weather";
+import Debug from "./pages/Debug";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/weather" element={<Weather />} />
-    </Routes>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/weather">Weather</Link>
+          </li>
+          <li>
+            <Link to="/debug">Debug</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/weather" element={<Weather />} />
+        <Route path="/debug" element={<Debug />} />
+      </Routes>
+    </div>
   );
 }
 
---
+++ korenan.react.client/src/pages/Debug.tsx
@@ -0,0 +1,11 @@
+
+function Debug() {
+    return (
+        <div>
+            <h1>Debug Page</h1>
+            <p>This is the debug page.</p>
+        </div>
+    );
+}
+
+export default Debug;
