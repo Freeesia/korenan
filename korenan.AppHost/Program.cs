9a0fb2a9dd67610eb1ffadd1abf70406f4dbb315
Freesia <freesia@studiofreesia.com>
2025-01-05 20:48:03 +0900

    brazer削除

--- korenan.AppHost/Program.cs
+++ korenan.AppHost/Program.cs
@@ -2,10 +2,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var apiService = builder.AddProject<Projects.korenan_ApiService>("apiservice");
 
-builder.AddProject<Projects.korenan_Web>("webfrontend")
-    .WithExternalHttpEndpoints()
-    .WithReference(apiService);
-
 builder.AddNpmApp("react", "../korenan.react.client", "dev")
     .WithReference(apiService)
     .WaitFor(apiService)
