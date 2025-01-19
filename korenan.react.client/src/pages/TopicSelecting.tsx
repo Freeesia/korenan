2e0708e0bd66d8fdae926fb2c0d042d222b9c695
Freesia <freesia@studiofreesia.com>
2025-01-19 20:32:57 +0900

    お題抽選中シーンの追加 #11 Fix

--- korenan.react.client/src/pages/TopicSelecting.tsx
+++ korenan.react.client/src/pages/TopicSelecting.tsx
@@ -1,7 +1,19 @@
+import { useEffect, useState } from "react";
+
 function TopicSelecting() {
+  const [dots, setDots] = useState("");
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDots((prev) => (prev.length < 3 ? prev + "." : ""));
+    }, 500);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="container">
-      <h1 className="title">お題抽選中</h1>
+      <h1 className="title">お題抽選中{dots}</h1>
     </div>
   );
 }
